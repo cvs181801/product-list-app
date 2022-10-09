3ebac50169e4ba5231fa58111b4074cb8cbbd1f8
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-10-09 10:57:47 -0700

    add approutercompnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './header/homepage/homepage.component';
 import { ProductListComponent } from './header/product-list/product-list.component';
 import { FilterByComponent } from './header/product-list/filter-by/filter-by.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,12 @@ import { FilterByComponent } from './header/product-list/filter-by/filter-by.com
     FilterByComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 
-//may need to add angular router afeter all
