05e3f891d090821cdcefc334867d50a487febded
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-10-09 12:34:34 -0700

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './header/homepage/homepage.component';
+import { ProductListComponent } from './header/product-list/product-list.component';
 
-
+const routes : Routes = [
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'product-list', component: ProductListComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
